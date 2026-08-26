435046579430e89c1bdad1ebed73507bd48f5a1e
Mihir Desai <madesai98@users.noreply.github.com>
2026-08-26 16:30:13 -0700

    Remove stale headless Web UI hook

--- cmd/tunnel-manager/desktop_nogui.go
+++ cmd/tunnel-manager/desktop_nogui.go
@@ -2,15 +2,9 @@
 
 package main
 
-import (
-	"context"
-
-	coreapp "github.com/madesai98/GPT-Tunnel-Manager/internal/app"
-	"github.com/madesai98/GPT-Tunnel-Manager/internal/platform"
-)
+import coreapp "github.com/madesai98/GPT-Tunnel-Manager/internal/app"
 
 func runDesktop(a *coreapp.App, setFocus func(func())) error {
-	if setFocus != nil { setFocus(func(){ _ = platform.OpenURL(context.Background(), a.AdminURL()) }) }
-	_ = platform.OpenURL(context.Background(), a.AdminURL())
+	_ = setFocus
 	return runHeadless(a)
 }
