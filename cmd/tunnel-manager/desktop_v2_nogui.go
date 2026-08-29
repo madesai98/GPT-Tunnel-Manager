45016935de8f7c8caf9271816f4c35f6029183dc
Mihir Desai <madesai98@users.noreply.github.com>
2026-08-29 14:05:56 -0700

    Phase 11: add headless v2 desktop seam

---
+++ cmd/tunnel-manager/desktop_v2_nogui.go
@@ -0,0 +1,10 @@
+//go:build nogui
+
+package main
+
+import coreapp "github.com/madesai98/GPT-Tunnel-Manager/internal/app"
+
+func runDesktopV2(a *coreapp.V2App, setFocus func(func())) error {
+	_ = setFocus
+	return runHeadlessV2(a)
+}
