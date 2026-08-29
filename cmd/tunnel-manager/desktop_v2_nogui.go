bb1ad2a5e3da65bc867df56f4ccb4f6e9f4f6283
Mihir Desai <madesai98@users.noreply.github.com>
2026-08-29 14:06:14 -0700

    Phase 11: fix headless v2 desktop seam

--- cmd/tunnel-manager/desktop_v2_nogui.go
+++ cmd/tunnel-manager/desktop_v2_nogui.go
@@ -6,5 +6,5 @@ import coreapp "github.com/madesai98/GPT-Tunnel-Manager/internal/app"
 
 func runDesktopV2(a *coreapp.V2App, setFocus func(func())) error {
 	_ = setFocus
-	return runHeadlessV2(a)
+	return runHeadless(a)
 }
